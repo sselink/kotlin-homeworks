064a5fc022d487e03a03007c53c50b525278cfcf
sselink <147547597+sselink@users.noreply.github.com>
2025-04-26 23:52:46 +0300

    Create homework1.kt

---
+++ homework-1/homework1.kt
@@ -0,0 +1,102 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+
+    var sehir: String = "Adana"
+    var ulke: String = "Turkiye"
+    var telefon: String = "+90 543 123 4567"
+    var postaKodu: String = "01200"
+    var email: String = "selin.selin@icloud.com"
+    var meslek: String = "Bilgisayar Muhendisi"
+    var stokMiktari: Int = 25
+    var musteriAdi: String = "Maya Su"
+    var bakiye: Double = 7500.55
+    var dogumGunu: String = "2022-04-14"
+    var maas: Double = 75500.00
+    var medeniDurum: String = "Bekar"
+    var urunYorum: String = "Miyav"
+    var odemeTarihi: String = "2025.04.18"
+    var odeme: Double = 255.89
+    var siparisAdeti: Int = 1
+    var arabaModeli: String = "Honda CR-V"
+    var kitapAdi: String = "The Lord of the Rings"
+    var yayinlamaTarihi: String = "1954-07-29"
+    var indirimMiktari: Double = 75.6
+    var odaSayisi: Int = 3
+    var enlem: Double = 34.2225
+    var boylam: Double = 36.4441
+    var urunAdi: String = "Kablosuz Mouse"
+    var yemekFiyati: Double = 169.99
+    var marka: String = "Apple"
+    var muzikAdi: String = "Rock You Like a Hurricane"
+    var videoSuresi: Int = 256
+    var urunPuani: Float = 4.3f
+    var resimAdi: String = "mama1.jpg"
+    var dosyaFormati: String = "PDF"
+    var renk: String = "Mor"
+    var renkKodu: String = "#660099"
+    var telefonModeli: String = "iPhone 16 Pro Max"
+    var ekranBoyutu: String = "6.86 inc"
+    var agirlik: Double = 227.0
+    var ulusalGun: String = "29 Ekim"
+    var tatilGunu: String = "2025.04.23"
+    var rezervasyonTarihi: String = "2025.04.22"
+    var sokakAdi: String = "Menekse"
+    var otobusHatti: String = "124"
+    var kalanDakika: Int = 3
+    var takipKodu: String = "543657"
+    var kuponSuresi: String = "2025.05.10"
+    var kuponKodu: String = "SELIN15"
+    var faturaAdresi: String = "Menekse Sk. No:16, Seyhan/Adana"
+
+
+    println("Sehir: $sehir")
+    println("Ulke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktari: $stokMiktari")
+    println("Musteri Adi: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Dogum Gunu: $dogumGunu")
+    println("Maas: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Urun Yorum: $urunYorum")
+    println("Odeme Tarihi: $odemeTarihi")
+    println("Odeme: $odeme")
+    println("Siparis Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adi: $kitapAdi")
+    println("Yayinlama Tarihi: $yayinlamaTarihi")
+    println("Indirim Miktari: $indirimMiktari")
+    println("Oda Sayisi: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Urun Adi: $urunAdi")
+    println("Yemek Fiyati: $yemekFiyati")
+    println("Marka: $marka")
+    println("Muzik Adi: $muzikAdi")
+    println("Video Suresi: $videoSuresi")
+    println("Urun Puani: $urunPuani")
+    println("Resim Adi: $resimAdi")
+    println("Dosya Formati: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Agirlik: $agirlik")
+    println("Ulusal Gun: $ulusalGun")
+    println("Tatil Gunu: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adi: $sokakAdi")
+    println("Otobus Hatti: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Suresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+
+
+
+}
